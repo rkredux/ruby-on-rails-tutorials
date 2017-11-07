2e4e7cf759831941df4da2724dfdc0cd582122de
rkredux <rkredux@gmail.com>
2017-11-07 07:29:07 +0000

    Add a Static Pages Controller

---
+++ app/helpers/gate_bound_helper.rb
@@ -0,0 +1,2 @@
+module GateBoundHelper
+end
---
+++ test/controllers/gate_bound_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GateBoundControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
